6be9684d9b21ec2663476eebd96378cf7d3e3139
nicolasbarriosg <barriosnicobarrios@gmail.com>
2024-10-25 19:02:05 -0300

    1

--- Gestion/settings.py
+++ Gestion/settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = "image/"
 MEDIA_ROOT = BASE_DIR/"media"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
