dc0e13f593fcfdaeb75f0ce1d7774975d5c73e57
nicobarriosg <barriosnicobarrios@gmail.com>
2024-11-07 22:02:44 -0300

    LISTOOOOOOOOOOO

--- moduloPeliculas/models.py
+++ moduloPeliculas/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.urls import reverse
 
 class Pelicula(models.Model):
     GENERO_CHOICES = [
@@ -31,12 +32,16 @@ class Pelicula(models.Model):
     año = models.PositiveIntegerField(help_text='Año de publicacion')
     duracion = models.PositiveIntegerField(help_text='Duración de la película en minutos')
     resena = models.TextField(blank=True, help_text='Breve reseña del contenido de la película')
-    imagen = models.URLField()  # Aquí se almacena la URL de la imagen
-    imagenbanner = models.URLField(blank=True, help_text='URL del banner de la imagen')  # Nuevo campo para la imagen del banner
-    imagenname = models.URLField(help_text='URL del nombre del archivo de la imagen', blank=True)  # Nuevo campo imagenname
+    imagen = models.URLField(help_text='URL de la imagen de la película')
+    imagenbanner = models.URLField(blank=True, help_text='URL del banner de la imagen')
+    imagenname = models.URLField(help_text='URL del nombre del archivo de la imagen', blank=True)
+    tipo = "Pelicula"
 
     def __str__(self):
         return self.nombre
+
+    def get_absolute_url(self):
+        return reverse('pelicula-detail', args=[str(self.id)])
 
 class Serie(models.Model):
     GENERO_CHOICES = [
@@ -69,9 +74,13 @@ class Serie(models.Model):
     temporadas = models.PositiveIntegerField(help_text='Número de temporadas')
     capitulos = models.PositiveIntegerField(help_text='Número de capítulos por temporada')
     resena = models.TextField(blank=True, help_text='Breve reseña del contenido de la serie')
-    imagen = models.URLField()  # Aquí se almacena la URL de la imagen
-    imagenbanner = models.URLField(blank=True, help_text='URL del banner de la imagen')  # Nuevo campo para la imagen del banner
-    imagenname = models.URLField(help_text='URL del nombre del archivo de la imagen', blank=True)  # Nuevo campo imagenname
+    imagen = models.URLField(help_text='URL de la imagen de la serie')
+    imagenbanner = models.URLField(blank=True, help_text='URL del banner de la imagen')
+    imagenname = models.URLField(help_text='URL del nombre del archivo de la imagen', blank=True)
+    tipo = "Serie"
 
     def __str__(self):
         return self.nombre
+
+    def get_absolute_url(self):
+        return reverse('serie-detail', args=[str(self.id)])
--- moduloPeliculas/views.py
+++ moduloPeliculas/views.py
@@ -50,38 +50,29 @@ def pagina_principal(request):
 
 
 
-
+############################### PELICULAS ###############################
 def PeliculaListView(request):
-
     peliculas = Pelicula.objects.all()
-
     peliculas = peliculas.order_by('nombre')
-
     peliculas = {"peliculas":peliculas}
-
     return render(request,"peliculas_list.html",peliculas)
 
 def PeliculaListCrud(request):
-
     peliculas = Pelicula.objects.all()
-
     peliculas = peliculas.order_by('nombre')
-
     peliculas = {"peliculas":peliculas}
-
     return render(request,"peliculas_crud.html",peliculas)
 
 def pelicula_detail(request, id):
     pelicula = get_object_or_404(Pelicula, id=id)
     return render(request, "pelicula_details.html", {'pelicula': pelicula})
 
-
 def agregar_pelicula(request):
     if request.method == 'POST':
         form = PeliculaForm(request.POST,request.FILES)
         if form.is_valid():
             form.save()
-            # Redirige a la vista lista_articulos
+
             return PeliculaListView(request)
     else:
         form = PeliculaForm()
@@ -91,7 +82,7 @@ def agregar_pelicula(request):
 def eliminar_pelicula(request, id):
     pelicula = Pelicula.objects.get(id=id)
     pelicula.delete()
-    # Agrega un mensaje de confirmación
+
     messages.success(
         request, f'Se ha eliminado a {pelicula.nombre}')
     return redirect('pagina_principal')
@@ -112,7 +103,7 @@ def editar_pelicula(request, id):
     return render(request, 'pelicula_form.html', {'form': form})
 
 
-
+############################### SERIES ###############################
 def SerieListView(request):
     series = Serie.objects.all()
     series = series.order_by('nombre')
